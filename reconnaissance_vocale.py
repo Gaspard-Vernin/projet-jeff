603cd43ab51905f01d9548a35317e652ab763abc
Gaspard-Vernin <gaspard.vernin@telecom-paris.fr>
2026-05-04 10:43:08 +0200

    changement spectre passe bande

--- reconnaissance_vocale.py
+++ reconnaissance_vocale.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib 
 
 def passe_bande(signal):
-    """coupe les frequences pour ne garder que le spectre audbile 20-20000 Hz"""
+    """coupe les frequences pour ne garder que le spectre de la voix 20-2000 Hz"""
     tf_signal = np.fft.fft(signal)
-    tf_signal[20000:]=0 
+    tf_signal[2000:]=0 
     tf_signal[:20]=0
     return np.fft.ifft(tf_signal)
