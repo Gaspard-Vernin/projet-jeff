4087da0e0d5c3de36dc9b5df57c5b7eefc46f511
Gaspard-Vernin <gaspard.vernin@telecom-paris.fr>
2026-05-04 11:38:26 +0200

    ajout d'une fonction backprop, forward et train

--- reconnaissance_vocale.py
+++ reconnaissance_vocale.py
@@ -5,6 +5,7 @@ import matplotlib
 class Network(torch.nn.Module):
     def __init__(self,dim_sortie=10,taille_out_channels=64):
         super().__init__() 
+        self.dim_sortie=dim_sortie
         self.conv_lay1 = torch.nn.Conv2d(in_channels=1,out_channels=taille_out_channels//2,padding=1,kernel_size=3)
         self.norm1 = torch.nn.BatchNorm2d(num_features=taille_out_channels//2)
         self.conv_lay2 = torch.nn.Conv2d(in_channels=1,out_channels=taille_out_channels,padding=1,kernel_size=3)
@@ -16,6 +17,7 @@ class Network(torch.nn.Module):
         self.fc2 = torch.nn.Linear(taille_out_channels,taille_out_channels//2)
         self.rel2=torch.nn.ReLU()
         self.fc3 = torch.nn.Linear(taille_out_channels//2,dim_sortie)
+        self.optimizer=torch.optim.Adam(self.parameters(),lr=1e-3)
     def forward(self,input):
         x=self.norm1(self.conv_lay1(input))
         x=self.norm2(self.conv_lay2(x))
@@ -25,4 +27,22 @@ class Network(torch.nn.Module):
         x=self.rel1(self.fc1(x))
         x=self.rel2(self.fc2(x))
         return self.fc3(x) 
-    
+    def backprop(self, resultat,output):
+        res = torch.zeros(self.dim_sortie)
+        res[resultat]=1 
+        loss = torch.nn.MSELoss()
+        val_loss = loss(output-resultat)
+        self.optimizer.zero_grad()
+        val_loss.backward()
+        self.optimizer.step()
+def train(dataset,nb_mots_differents_dataset,nb_epoch=3):
+    """créer et entraine reseau de detections de mots,
+        dataset est de la forme [(signal,numero du mot représenté)]
+    """
+    net = Network(dim_sortie=nb_mots_differents_dataset)
+    for _ in range(nb_epoch):
+        for (signal,num) in dataset:
+            forw = net(signal)
+            net.backprop(num,forw)
+    return net
+        
