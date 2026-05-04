1022355a3d67bc7431dbdc91c2eb0e0c81a9b88a
Gaspard-Vernin <gaspard.vernin@telecom-paris.fr>
2026-05-04 10:41:41 +0200

    add passe bande

--- reconnaissance_vocale.py
+++ reconnaissance_vocale.py
@@ -1,1 +1,10 @@
-import torch
+import torch 
+import numpy as np
+import matplotlib 
+
+def passe_bande(signal):
+    """coupe les frequences pour ne garder que le spectre audbile 20-20000 Hz"""
+    tf_signal = np.fft.fft(signal)
+    tf_signal[20000:]=0 
+    tf_signal[:20]=0
+    return np.fft.ifft(tf_signal)
